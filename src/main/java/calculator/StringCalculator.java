ec008bce2a1c28ad4cae320a90c6d6b46af0d797
aslam sayyed <aslamsayyed2013@gmail.com>
2021-10-17 21:29:28 +0530

    NewLine input done

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,7 +1,7 @@
 package calculator;
 
 class StringCalculator {
-
+/*
     public int add1(String input) {
     	String[] numbers=input.split(delimiter);
     	int sum=0;
@@ -63,5 +63,50 @@ class StringCalculator {
 			
 			sum=sum+Integer.parseInt(word);
 	}
-    
+   */ 
+	public static int add(String text){
+	       if(text.equals("")){
+	           return 0;
+	       }
+	       else{
+	           String delimiter = ",";
+	           if(text.matches("//(.)\n(.)")){
+	               delimiter = Character.toString(text.charAt(2));
+	               text = text.substring(4);
+	           }
+	           
+	           String numList[] = splitNumbers(text, delimiter + "|\n");
+	           return sum(numList);
+	       }
+	   }
+
+	   private static int toInt(String number){
+	       return Integer.parseInt(number);
+	   }
+
+	   private static String[] splitNumbers(String numbers, String divider){
+	       return numbers.split(divider);
+	   }
+
+	   private static int sum(String[] numbers){
+	       int total = 0;
+	       String negString = "";
+
+	        for(String number : numbers){
+	           if(toInt(number) < 0){
+	               if(negString.equals(""))
+	                   negString = number;
+	               else
+	                   negString += ("," + number);
+	           }
+	           if(toInt(number) < 1000)
+	               total += toInt(number);
+	       }
+
+	       if(!negString.equals("")){
+	           throw new IllegalArgumentException("Negatives not allowed: " + negString);
+	       }
+
+	       return total;
+	    }
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
-
+/*
     @Test
     void emptyString() {
         StringCalculator stringCalculator = new StringCalculator();
@@ -37,7 +37,58 @@ class StringCalculatorShould {
     public void negativeInputException() throws Exception{
     	String.add("-1,2");
     }
+*/
+	@Test
+	   public void testEmptyString() {
+	       assertEquals(0, StringCalculator.add(""));
+	   }
 
+	   @Test
+	   public void testOneNumber() {
+	       assertEquals(1, StringCalculator.add("1"));
+	   }
+
+	   @Test
+	   public void testTwoNumbers(){
+	       assertEquals(3, StringCalculator.add("1,2"));
+	   }
+
+	   @Test
+	    public void testThreeNumbers(){
+	       assertEquals(6, StringCalculator.add("1,2,3"));
+	    }
+
+	    @Test
+	    public void testNewLine(){
+	       assertEquals(6, StringCalculator.add("1\n2,3"));
+	    }
+
+	    @Test
+	    public void testNegativeNumver(){
+	       try {
+	           StringCalculator.add("-1,2");
+	       }
+	       catch (IllegalArgumentException e){
+	           assertEquals(e.getMessage(), "Negatives not allowed: -1");
+	       }
+
+	       try {
+	           StringCalculator.add("2,-4,3,-5");
+	       }
+	       catch (IllegalArgumentException e){
+	           assertEquals(e.getMessage(), "Negatives not allowed: -4,-5");
+	       }
+	    }
+
+	    @Test
+	    public void testOverThousand(){
+	       assertEquals(2, StringCalculator.add("1000,2"));
+	    }
+
+	    @Test
+	    public void testOtherDelimiter(){
+	       assertEquals(3, StringCalculator.add("//;\n1;2"));
+	    }
    
 }
 
